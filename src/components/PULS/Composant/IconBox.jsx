a321d8fa419c067680531752b4a3871f126c6b75
ekouakou <ekouakou1er@gmail.com>
2025-10-30 10:28:31 +0000

    Cporrection de la page inscription

--- src/components/PULS/Composant/IconBox.jsx
+++ src/components/PULS/Composant/IconBox.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const IconBox = ({ icon: Icon, size = 18, color = "#10b981" }) => {
   const academicYear = JSON.parse(localStorage.getItem('academicYearMain'));
   return (
-    <div className={`IconBox-${academicYear.niveauEnseignement?.libelle.replace(/[\s()]/g, '')} IconBox-niveauEnseignement-${academicYear.niveauEnseignement?.id}`} >
+    <div className={`IconBox-${academicYear?.niveauEnseignement?.libelle.replace(/[\s()]/g, '')} IconBox-niveauEnseignement-${academicYear?.niveauEnseignement?.id}`} >
       <Icon size={size}  />
     </div>
   );
